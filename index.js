07becfaca251f128232822c91645f807239c61f3
EllifasEmanuel <elifas.emanuel@versatecnologia.com.br>
2022-10-21 13:30:44 -0300

    Validando erro de arquivo não encontrado.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado.'));
+}
+
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro)
+            trataErro(erro);
+        
         console.log(chalk.blue(texto));
-    })
+    });
 }
 
 pegaArquivo('./arquivos/texto.md');
