514173e1f54ac7cf8cee29c77a3884c75223f099
EllifasEmanuel <elifas.emanuel@versatecnologia.com.br>
2022-10-21 15:13:27 -0300

    Adicionando função assicrono

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ function trataErro(erro){
 
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if(erro)
-            trataErro(erro);
-        
-        console.log(chalk.blue(texto));
-    });
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro);
 }
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if(erro)
+//             trataErro(erro);
+        
+//         console.log(chalk.blue(texto));
+//     });
+// }
 
 pegaArquivo('./arquivos/texto.md');
