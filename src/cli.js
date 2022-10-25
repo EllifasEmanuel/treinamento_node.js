5f8645a27a192fc1f855d9c55d117f332370d48f
EllifasEmanuel <elifas.emanuel@versatecnologia.com.br>
2022-10-25 17:11:41 -0300

    Criando arquivo e função para validar lista

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,21 @@
 import pegaArquivo from "./index.js";
 import fs from 'fs';
 import chalk from 'chalk';
+import listaValidada from "./http-validacao.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado, caminho = ''){
-    console.log(chalk.green(`Lista de links do arquivo ${caminho}:`),resultado);
+function imprimeLista(valida, resultado, caminho = ''){
+    if (valida) {
+        console.log(chalk.green(`Lista de links do arquivo ${caminho}:`),listaValidada(resultado));
+    } else {
+        console.log(chalk.green(`Lista de links do arquivo ${caminho}:`),resultado);
+    }
 }
 
 async function processaTexto(argumentos){
     const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
     try{
         fs.lstatSync(caminho);
     } catch(erro){
@@ -20,14 +26,14 @@ async function processaTexto(argumentos){
     }
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(caminho);
-        imprimeLista(resultado);
+        imprimeLista(valida, resultado);
     } 
     else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeDeArquivo) => {
             const caminhoArquivo = `${caminho}/${nomeDeArquivo}`;
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
-            imprimeLista(lista, caminhoArquivo);
+            imprimeLista(valida, lista, caminhoArquivo);
         })
     }
 }
