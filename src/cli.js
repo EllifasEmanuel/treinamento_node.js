3b94769d52350a20b56c0eecf70d8f2d2eb5a49b
EllifasEmanuel <elifas.emanuel@versatecnologia.com.br>
2022-10-25 16:51:23 -0300

    Criando funcionalidade para diretórios e tratamento de erros

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,35 @@
 import pegaArquivo from "./index.js";
+import fs from 'fs';
 import chalk from 'chalk';
 
 const caminho = process.argv;
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.green('Lista de links:'),resultado);
+function imprimeLista(resultado, caminho = ''){
+    console.log(chalk.green(`Lista de links do arquivo ${caminho}:`),resultado);
+}
+
+async function processaTexto(argumentos){
+    const caminho = argumentos[2];
+    try{
+        fs.lstatSync(caminho);
+    } catch(erro){
+        if(erro.code === "ENOENT"){
+            console.log(chalk.red(`Arquivo ou diretório não encontrado: ${caminho}.`));
+            return; 
+        }
+    }
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(caminho);
+        imprimeLista(resultado);
+    } 
+    else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const caminhoArquivo = `${caminho}/${nomeDeArquivo}`;
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);
+            imprimeLista(lista, caminhoArquivo);
+        })
+    }
 }
 
 processaTexto(caminho);
